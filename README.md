54bf272d09ae882f4803f34911ef11ab5725f4e2
Aki Pandzic <aki.pandzic64@gmail.com>
2026-04-11 12:27:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+Hrana
+
+Kategorije
+Voce
+    jabuke
+    banane
+    narance
+    sljive
+    jagode
+
+Povrce
+    spinat
+    luk
+    gljive
+    kupus
+    poriluk
+
+Riba
+    brancin
+    tuna
+    losos
+    orada
+    skusa
+
+Meso
+    pileci file
+    pileci batak zabatak
+    svinjska vratina
+    juneci ramstek
+    hrenovke
+
+Grickalice
+    cips
+    smoki
+    kokice
+    stapici
+    tortilja cips
+
+Kruh
+    perec
+    kukuruzni
+    integralni
+    kroasan
+    krafna
+
+Konzervirano
+    grah
+    kukuruz
+    paradajz
+    sardine
+    tuna
+
+Mlijecni proizvodi
+    mlijeko
+    jogurt
+    mozarella
+    puding
+    camembert sir
+
+Orasasti plodovi
+    kikiriki
+    orasi
+    bademi
+    indijski orascici
+    ljesnjaci
+
+Zacini
+    sol
+    papar
+    cesnjak u prahu
+    paprika
+    kajenski papar
+
---
+++ assets/scripts/data.js
@@ -0,0 +1,224 @@
+const data = {
+    "website": "Hrana",
+    "categories": [
+        { 
+            "name" : "Grickalice",
+            "image" : "grickalice.jpg",
+            "products" : [
+                { 
+                    "name" : "Cips", "image" : "cips.jpg"
+                },
+                { 
+                    "name" : "Kokice", "image" : "popcorn.jpg"
+                },                
+                { 
+                    "name" : "Smoki", "image" : "smoki.jpg"
+                },
+                { 
+                    "name" : "Stapici", "image" : "stapici.jpg"
+                }, 
+                { 
+                    "name" : "Tortilja Cips", "image" : "tortilja_cips.jpg"
+                }
+            ]
+        },
+        
+        { 
+            "name" : "konzervirano",
+            "image" : "konzervirano.jpg",
+            "products" : [
+                { 
+                    "name" : "Konzerva Kukuruza", "image" : "corn.jpg"
+                },
+                { 
+                    "name" : "Konzerva Graha", "image" : "grah.jpg"
+                },                
+                { 
+                    "name" : "Konzerva Paradajza", "image" : "paradajz.jpg"
+                },
+                { 
+                    "name" : "Konzerva Sardina", "image" : "sardine.jpg"
+                }, 
+                { 
+                    "name" : "Konzerva Tune", "image" : "tuna.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Kruh",
+            "image" : "kruh.jpg",
+            "products" : [
+                { 
+                    "name" : "Integralni Kruh", "image" : "integralni.jpg"
+                },
+                { 
+                    "name" : "Krafna", "image" : "krafna.jpg"
+                },                
+                { 
+                    "name" : "Kroasan", "image" : "kroasan.jpg"
+                },
+                { 
+                    "name" : "Kukuruzni kruh", "image" : "kukuruzni.jpg"
+                }, 
+                { 
+                    "name" : "Perec", "image" : "perec.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Meso",
+            "image" : "meso.jpg",
+            "products" : [
+                { 
+                    "name" : "Hrenovke", "image" : "hrenovke.jpg"
+                },
+                { 
+                    "name" : "Juneci Ramstek", "image" : "juneci_ramstek.jpg"
+                },                
+                { 
+                    "name" : "Pileci Batak i Zabatak", "image" : "pileci_batak_zabatak.jpg"
+                },
+                { 
+                    "name" : "Pileci File", "image" : "pilci_file.jpg"
+                }, 
+                { 
+                    "name" : "Svinjska Vratina", "image" : "svinjska_vratina.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Mlijecni Proizvodi",
+            "image" : "mlijecni_proizvodi.jpg",
+            "products" : [
+                { 
+                    "name" : "Sir Camembert", "image" : "camembert.jpg"
+                },
+                { 
+                    "name" : "Tekuci Jogurt", "image" : "jogurt.jpg"
+                },                
+                { 
+                    "name" : "Mlijeko", "image" : "mlijeko.jpg"
+                },
+                { 
+                    "name" : "Sir Mozzarella", "image" : "mozzarella.jpg"
+                }, 
+                { 
+                    "name" : "Vocni Puding", "image" : "puding.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Orasasti Plodovi",
+            "image" : "orasasti_plodovi.jpg",
+            "products" : [
+                { 
+                    "name" : "Bademi", "image" : "bademi.jpg"
+                },
+                { 
+                    "name" : "Indijski Orascici", "image" : "indijski_orascici.jpg"
+                },                
+                { 
+                    "name" : "Kikiriki", "image" : "kikiriki.jpg"
+                },
+                { 
+                    "name" : "Ljesnjaci", "image" : "ljesnjaci.jpg"
+                }, 
+                { 
+                    "name" : "Orasi", "image" : "orasi.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Povrce",
+            "image" : "povrce.jpg",
+            "products" : [
+                { 
+                    "name" : "Gljive", "image" : "gljive.jpg"
+                },
+                { 
+                    "name" : "Kupus", "image" : "kupus.jpg"
+                },                
+                { 
+                    "name" : "Poriluk", "image" : "poriluk.jpg"
+                },
+                { 
+                    "name" : "Luk", "image" : "luk.jpg"
+                }, 
+                { 
+                    "name" : "Spinat", "image" : "spinat.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Riba",
+            "image" : "riba.jpg",
+            "products" : [
+                { 
+                    "name" : "Brancin", "image" : "brancin.jpg"
+                },
+                { 
+                    "name" : "Losos", "image" : "losos.jpg"
+                },                
+                { 
+                    "name" : "Tuna", "image" : "tuna.jpg"
+                },
+                { 
+                    "name" : "Orada", "image" : "orada.jpg"
+                }, 
+                { 
+                    "name" : "Skusa", "image" : "skusa.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Voce",
+            "image" : "voce.jpg",
+            "products" : [
+                { 
+                    "name" : "Banane", "image" : "banane.jpg"
+                },
+                { 
+                    "name" : "Jabuke", "image" : "jabuke.jpg"
+                },                
+                { 
+                    "name" : "Jagode", "image" : "jagode.jpg"
+                },
+                { 
+                    "name" : "Narance", "image" : "narance.jpg"
+                }, 
+                { 
+                    "name" : "Sljive", "image" : "sljive.jpg"
+                }
+            ]
+        },
+
+        { 
+            "name" : "Zacini",
+            "image" : "zacini.jpg",
+            "products" : [
+                { 
+                    "name" : "Cesnjak u prahu", "image" : "cesnjak_prah.jpg"
+                },
+                { 
+                    "name" : "Kajenski Papar", "image" : "kajenski_papar.jpg"
+                },                
+                { 
+                    "name" : "Papar", "image" : "papar.jpg"
+                },
+                { 
+                    "name" : "Paprika", "image" : "paprika.jpg"
+                }, 
+                { 
+                    "name" : "Sol", "image" : "sol.jpg"
+                }
+            ]
+        }
+    ]
+}
